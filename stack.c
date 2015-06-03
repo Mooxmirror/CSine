021c6f55e8f6c2376d8d5a571666d7b2321aa6ba
Lennart Espe <lennart.espe@gmail.com>
2015-06-03 20:46:47 +0200

    refactor: stack
    Refactor stack for better readability, add STACK_AUTOFREE for working
    with pointers

--- stack.c
+++ stack.c
@@ -5,77 +5,104 @@ A Stack structure implementation
 
 #include "stack.h"
 
-/*
-Initializes a new empty stack.
-*/
+// Initializes a new empty stack.
 Stack * Stack_create()
 {
-	Stack * s = (Stack *) malloc(sizeof(Stack));
-	s->top = NULL;
-	return s;
+	// Allocate new stack memory
+	Stack * stack_ptr = (Stack *) malloc(sizeof(Stack));
+	
+	// No memory available!
+	if (stack_ptr == NULL) return;
+	
+	// Set first element to NULL
+	stack_ptr->top = NULL;
+	return stack_ptr;
 }
 
-/*
-Frees the memory and destroys the reference.
-*/
-void Stack_destroy(Stack * s)
+// Frees the memory and destroys the reference.
+void Stack_destroy(Stack * stack_ptr)
 {
-	Stack_item * current = s->top;
-	while (current != NULL) {
-		Stack_item * next = current->next;
-		free(current);
-		current = next;
+	// Get the first item
+	Stack_item * item_ptr = stack_ptr->top;
+	
+	// Iterate till ...
+	while (item_ptr != NULL) {
+		// Get the next element
+		Stack_item * next_ptr = item_ptr->next;
+		
+		// Clear the value pointer
+		if (STACK_AUTOFREE) free(item_ptr->value);
+		
+		// Delete the current element
+		free(item_ptr);
+		
+		// Set current element to next
+		item_ptr = next_ptr;
 	}
-	free(s);
+	// everything is deleted.
+	free(stack_ptr);
 }
 
-/*
-Peeks the top value from the stack. Does not remove the item.
-*/
-Stack_value Stack_peek(Stack * s)
+// Peeks the top value from the stack. Does not remove the item.
+Stack_value Stack_peek(Stack * stack_ptr)
 {
-	Stack_item * top = s->top;
-	if (top != NULL)
-		return top->value;
+	// Get the first item
+	Stack_item * item_ptr = stack_ptr->top;
+	
+	// Return the value if it exists!
+	if (item_ptr != NULL)
+		return item_ptr->value;
 	
 	return STACK_VALUE_NULL;
 }
-/*
-Pulls the top value from the stack. Removes the item.
-*/
-Stack_value Stack_pull(Stack * s)
+// Pulls the top value from the stack. Removes the item.
+Stack_value Stack_pull(Stack * stack_ptr)
 {
-	Stack_item * top = s->top;
+	// Get the first element
+	Stack_item * item_ptr = stack_ptr->top;
 	
-	if (top != NULL)
+	// if the item exists!
+	if (item_ptr != NULL)
 	{
-		Stack_value value = top->value;
-		Stack_item * tmp = top->next;
-		s->top = tmp;
-		free(top);
+		// Get the value
+		Stack_value value = item_ptr->value;
 		
+		// Save the next element
+		Stack_item * tmp_ptr = item_ptr->next;
+		
+		// Take the current element from the top
+		stack_ptr->top = tmp_ptr;
+		
+		// Delete it
+		free(item_ptr);
 		return value;
 	}
 	
 	return STACK_VALUE_NULL;
 }
 
-/*
-Puts the value onto the stack.
-*/
-void Stack_put(Stack * s, Stack_value value)
+// Puts the value onto the stack.
+void Stack_push(Stack * stack_ptr, Stack_value value)
 {
-	Stack_item * top = s->top;
-	Stack_item * next = malloc(sizeof(Stack_item));
-	next->value = value;
-	next->next = top;
-	s->top = next;
+	// Get the first element
+	Stack_item * item_ptr = stack_ptr->top;
+	
+	// Allocate a memory for the new item
+	Stack_item * new_ptr = malloc(sizeof(Stack_item));
+
+	// No memory!
+	if (new_ptr == NULL) return;
+	
+	// Push the values
+	new_ptr->value = value;
+	new_ptr->next = item_ptr;
+	
+	// Put it on the top
+	stack_ptr->top = new_ptr;
 }
 
-/*
-Returns true if the top item is NULL.
-*/
-bool Stack_empty(Stack * s)
+// Returns true if the top item is NULL.
+bool Stack_empty(Stack * stack_ptr)
 {
-	return s->top == NULL;
+	return stack_ptr->top == NULL;
 }
